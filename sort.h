a6853b149ae29d35886839f26e94dd650c865019
ReduceTime <765422564@qq.com>
2014-09-22 12:22:29 +0800

    重新定义merge sort的接口函数，使mergeSort()接口与其他排序函数相同

--- sort.h
+++ sort.h
@@ -9,13 +9,14 @@ class sort
 {
 public:
 	sort(const vector<T1>&);
-	const sort<T1>& Display(ostream& out)const;
+	bool Display(ostream& out)const;//排序结果以console进行输出
+	bool Display(ofstream& out)const;//排序结果保存在文本文件中
 	bool insertSort();
 	bool selectSort();
-	bool mergeSort(int,int);
-	bool print_to_file(ofstream&)const;
+	bool mergeSort();
 private:
 	vector<T1> v;
 	bool merge(int p, int q, int r);
+	bool submergeSort(int, int);
 };
 #endif
